43d31dfb973851adc14f0c3efe8380eb06e24be5
Anastasiia Panchenko <anastasia-mary@mail.ru>
2014-11-29 16:50:22 -0500

    Update wsgi.py

--- django_project_template/django_project_template/wsgi.py
+++ django_project_template/django_project_template/wsgi.py
@@ -3,7 +3,7 @@ from os.path import abspath, dirname
 from sys import path
 
 
-sSITE_ROOT = dirname(dirname(abspath(__file__)))
+SITE_ROOT = dirname(dirname(abspath(__file__)))
 path.append(SITE_ROOT)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project_template.settings.production")
